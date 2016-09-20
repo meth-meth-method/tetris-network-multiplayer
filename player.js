b29f514458bd89b9150d7b6d8c309309cb74d38a
Pontus Persson <pontus.alexander@gmail.com>
2016-09-20 19:09:09 +0200

    Move createPiece into Player class.

--- player.js
+++ player.js
@@ -18,6 +18,53 @@ class Player
         this.reset();
     }
 
+    createPiece(type)
+    {
+        if (type === 'T') {
+            return [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 1, 0],
+            ];
+        } else if (type === 'O') {
+            return [
+                [2, 2],
+                [2, 2],
+            ];
+        } else if (type === 'L') {
+            return [
+                [0, 3, 0],
+                [0, 3, 0],
+                [0, 3, 3],
+            ];
+        } else if (type === 'J') {
+            return [
+                [0, 4, 0],
+                [0, 4, 0],
+                [4, 4, 0],
+            ];
+        } else if (type === 'I') {
+            return [
+                [0, 5, 0, 0],
+                [0, 5, 0, 0],
+                [0, 5, 0, 0],
+                [0, 5, 0, 0],
+            ];
+        } else if (type === 'S') {
+            return [
+                [0, 6, 6],
+                [6, 6, 0],
+                [0, 0, 0],
+            ];
+        } else if (type === 'Z') {
+            return [
+                [7, 7, 0],
+                [0, 7, 7],
+                [0, 0, 0],
+            ];
+        }
+    }
+
     drop()
     {
         this.pos.y++;
@@ -42,7 +89,7 @@ class Player
     reset()
     {
         const pieces = 'ILJOTSZ';
-        this.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+        this.matrix = this.createPiece(pieces[pieces.length * Math.random() | 0]);
         this.pos.y = 0;
         this.pos.x = (this.arena.matrix[0].length / 2 | 0) -
                      (this.matrix[0].length / 2 | 0);
