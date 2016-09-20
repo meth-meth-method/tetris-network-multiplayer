023172e70b4c98134d343b5fdabc6a0f7f39f95e
Pontus Persson <pom@spotify.com>
2016-09-20 16:47:58 +0200

    Add some cleanup and structuring.

--- game-controller.js
+++ game-controller.js
@@ -12,25 +12,8 @@ class GameController
 
         this.connection = new WebSocket('ws://localhost:8080');
         this.connection.addEventListener('message', event => {
-            console.log('Received message', event, event.data);
-            const msg = JSON.parse(event.data);
-
-            if (msg.type === 'session-id') {
-                window.location.hash = msg.data;
-            } else if (msg.type === 'join-session') {
-
-            } else if (msg.type === 'players-available') {
-                console.log(msg);
-                msg.data.others.forEach(playerId => {
-                    if (!this.playerMap.has(playerId)) {
-                        const tetris = this.addPlayer();
-                        this.playerMap.set(playerId, tetris);
-                    }
-                });
-            } else if (msg.type.startsWith('player-')) {
-                this.handlePlayerMessage(msg)
-
-            }
+            console.log('Received message', event.data);
+            this.handleMessage(JSON.parse(event.data));
         });
         this.connection.addEventListener('open', () => {
             console.log('Connection open');
@@ -42,27 +25,63 @@ class GameController
                 this.send('create-session', null);
             }
 
-            const host = this.tetri[0];
-            const player = host.player;
+            const tetris = this.tetri[0];
+            const player = tetris.player;
             player.event.listen('position-update', () => {
-                this.send('player-position', player.pos);
+                this.sendPlayerPosition(player);
             });
             player.event.listen('matrix-update', () => {
-                this.send('player-matrix', player.matrix);
+                this.sendPlayerMatrix(player);
             });
             player.event.listen('arena-update', () => {
-                this.send('player-arena', host.arena.matrix);
+                this.sendArenaMatrix(tetris.arena);
             });
         });
     }
 
     addPlayer()
     {
-        const element = document.importNode(this.template.content, true);
+        const element = document.importNode(this.template.content, true).children[0];
         const tetris = new Tetris(element);
         this.tetri.push(tetris);
         this.playerElements.appendChild(tetris.element);
         return tetris;
+    }
+
+    removePlayer(tetris)
+    {
+        this.playerElements.removeChild(tetris.element);
+    }
+
+    handleMessage(msg)
+    {
+        if (msg.type === 'session-id') {
+            window.location.hash = msg.data;
+        } else if (msg.type === 'join-session') {
+            this.sendGameState(this.tetri[0]);
+        } else if (msg.type === 'players-available') {
+            const me = msg.data.you;
+            const players = msg.data.players;
+            players.forEach(playerId => {
+                if (playerId === me) {
+                    return;
+                }
+
+                if (!this.playerMap.has(playerId)) {
+                    const tetris = this.addPlayer();
+                    this.playerMap.set(playerId, tetris);
+                }
+            });
+
+            [...this.playerMap.entries()].forEach(([id, tetris]) => {
+                if (players.indexOf(id) === -1) {
+                    this.removePlayer(tetris);
+                    this.playerMap.delete(id);
+                }
+            });
+        } else if (msg.type.startsWith('player-')) {
+            this.handlePlayerMessage(msg)
+        }
     }
 
     handlePlayerMessage(msg)
@@ -82,6 +101,28 @@ class GameController
             tetris.arena.matrix = msg.data;
         }
         tetris.draw();
+    }
+
+    sendGameState(tetris)
+    {
+        this.sendArenaMatrix(tetris.arena);
+        this.sendPlayerPosition(tetris.player);
+        this.sendPlayerMatrix(tetris.player);
+    }
+
+    sendArenaMatrix(arena)
+    {
+        this.send('player-arena', arena.matrix);
+    }
+
+    sendPlayerPosition(player)
+    {
+        this.send('player-position', player.pos);
+    }
+
+    sendPlayerMatrix(player)
+    {
+        this.send('player-matrix', player.matrix);
     }
 
     send(type, data)
--- server.js
+++ server.js
@@ -2,26 +2,39 @@ const WebSocketServer = require('ws').Server;
 
 const server = new WebSocketServer({port: 8080});
 
-function createId() {
-    const chars = 'abcdefghjkmnopqrstvwxyz01234567890';
-    let len = 6;
-    let id = '';
-    while (len--) {
-        id += chars[Math.random() * chars.length | 0];
-    }
-    return id;
-}
+const players = new Map;
+const sessions = new Map;
 
 class Session
 {
-    constructor()
+    constructor(id)
     {
-        this.id = createId();
+        this.id = id;
         this.players = new Set;
+    }
+    leave(player)
+    {
+        this.players.delete(player);
+    }
+    join(player)
+    {
+        if (this.players.has(player)) {
+            console.warn('Player already in session', this, player);
+            return;
+        }
+
+        this.players.add(player);
+        player.session = this;
+        player.send({
+            type: 'session-id',
+            data: this.id,
+        });
     }
     cast(msg)
     {
-        this.players.forEach(player => player.send(msg));
+        this.players.forEach(player => {
+            player.send(msg);
+        });
     }
 }
 
@@ -32,6 +45,16 @@ class Player
         this.id = createId();
         this.conn = conn;
         this.session = null;
+
+        this.state = {
+            arena: {
+                matrix: [],
+            },
+            player: {
+                pos: {x: 0, y: 0},
+                matrix: [],
+            },
+        };
     }
     cast(msg)
     {
@@ -41,7 +64,6 @@ class Player
         }
 
         msg.sender = this.id;
-        console.log('Broadcasting message', msg);
 
         this.session.players.forEach(player => {
             if (player === this) {
@@ -53,8 +75,8 @@ class Player
     send(msg)
     {
         const json = JSON.stringify(msg);
-        console.log('Sending message to', this.id, json);
-        this.conn.send(json, (err) => {
+        console.log(`Sending message from ${this.id} ${json}`);
+        this.conn.send(json, function ack(err) {
             if (err) {
                 console.error('Error sending', json, err);
             }
@@ -62,8 +84,45 @@ class Player
     }
 }
 
-const players = new Map;
-const sessions = new Map;
+function createId() {
+    const chars = 'abcdefghjkmnopqrstvwxyz01234567890';
+    let len = 6;
+    let id = '';
+    while (len--) {
+        id += chars[Math.random() * chars.length | 0];
+    }
+    return id;
+}
+
+function createSession(id = createId()) {
+    if (sessions.has(id)) {
+        throw new Error(`Session ${id} already exists`);
+    }
+
+    const session = new Session(id);
+    console.log('Creating session', session);
+
+    sessions.set(id, session);
+
+    return session;
+}
+
+function getSession(id) {
+    return sessions.get(id);
+}
+
+function broadcastSessionSync(session) {
+    const players = [...session.players];
+    players.forEach(player => {
+        player.send({
+            type: 'players-available',
+            data: {
+                you: player.id,
+                players: players.map(p => p.id),
+            },
+        });
+    });
+}
 
 server.on('connection', conn => {
     const player = new Player(conn);
@@ -73,43 +132,37 @@ server.on('connection', conn => {
 
     conn.on('message', msg => {
         console.log('Incoming message', msg);
-        const {type, data} = JSON.parse(msg);
-        if (type === 'create-session') {
-            const session = new Session();
-            console.log('Creating session', session);
-            session.players.add(player);
-            player.session = session;
-            sessions.set(session.id, session);
-            player.send({
-                type: 'session-id',
-                data: session.id,
-            });
-        } else if (type === 'join-session') {
-            const sessionId = data;
-            if (!sessions.has(sessionId)) {
-                console.warn('Tried to join unstarted session', sessionId);
-                return;
-            }
-            const session = sessions.get(sessionId);
-            console.log('Joining session', session);
-            session.players.add(player);
-            player.session = session;
 
-            const players = [...session.players];
-            players.forEach(player => {
-                player.send({
-                    type: 'players-available',
-                    data: {
-                        you: player.id,
-                        others: players.filter(p => p !== player).map(p => p.id),
-                    },
-                });
-            });
+        const {type, data} = JSON.parse(msg);
+
+        if (type === 'create-session') {
+
+            const session = createSession();
+            session.join(player);
+
+        } else if (type === 'join-session') {
+
+            const session =  getSession(data) || createSession(data);
+            session.join(player);
+            broadcastSessionSync(session);
+
         } else if (type.startsWith('player-')) {
+
             const player = players.get(conn);
-            if (player.session) {
-                player.cast({type, data});
-            }
+            player.cast({type, data});
+
+        }
+    });
+
+    conn.on('close', function close() {
+        console.log('Connection closed', player.id);
+        const session = player.session;
+        players.delete(conn);
+        session.leave(player);
+        broadcastSessionSync(session);
+        if (session.players.size === 0) {
+            console.log('Session empty, deleting', session);
+            sessions.delete(session);
         }
     });
 });
