b7a27d1e01c36b07054f5deec77a54441253a26f
Pontus Persson <pontus.alexander@gmail.com>
2016-09-20 19:38:38 +0200

    Add createPlayer method to TetrisManager.
    Create Tetris instances via console.
    `tetrisManager.createPlayer()`

--- tetris-manager.js
+++ tetris-manager.js
@@ -13,4 +13,17 @@ class TetrisManager
             this.tetri.push(tetris);
         });
     }
+
+    createPlayer()
+    {
+        const element = document
+            .importNode(this.template.content, true)
+            .children[0];
+
+        const tetris = new Tetris(element);
+
+        this.document.body.appendChild(tetris.element);
+
+        return tetris;
+    }
 }
