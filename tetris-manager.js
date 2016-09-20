e2dd5f7f2d6269e9057bc971dd4b87737ec46f69
Pontus Persson <pontus.alexander@gmail.com>
2016-09-20 19:45:45 +0200

    Remove original player setup routine.

--- tetris-manager.js
+++ tetris-manager.js
@@ -6,12 +6,6 @@ class TetrisManager
         this.template = this.document.querySelector('#player-template');
 
         this.tetri = [];
-
-        const playerElements = this.document.querySelectorAll('.player');
-        [...playerElements].forEach(element => {
-            const tetris = new Tetris(element);
-            this.tetri.push(tetris);
-        });
     }
 
     createPlayer()
