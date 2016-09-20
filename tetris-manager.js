3f1a4d48ccc6136f6338cda48e6d8ab2ca9b98f2
Pontus Persson <pontus.alexander@gmail.com>
2016-09-20 19:31:22 +0200

    Get template node and console.log it.
    Show #document-fragment.

--- tetris-manager.js
+++ tetris-manager.js
@@ -3,11 +3,13 @@ class TetrisManager
     constructor(document)
     {
         this.document = document;
+        this.template = this.document.querySelector('#player-template');
+        console.log(this.template);
+
         this.tetri = [];
 
         const playerElements = this.document.querySelectorAll('.player');
         [...playerElements].forEach(element => {
-            console.log(element);
             const tetris = new Tetris(element);
             this.tetri.push(tetris);
         });
