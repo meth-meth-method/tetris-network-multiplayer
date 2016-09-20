28c92843a669f83b0978738a5c4d73bd21a0c6a9
Pontus Persson <pontus.alexander@gmail.com>
2016-09-20 20:13:16 +0200

    Handle Open and Close Connection.

--- scripts/tetris-server.js
+++ scripts/tetris-server.js
@@ -1,3 +1,11 @@
 const WebSocketServer = require('ws').Server;
 
 const server = new WebSocketServer({port: 9000});
+
+server.on('connection', conn => {
+    console.log('Connection established');
+
+    conn.on('close', () => {
+        console.log('Connection closed');
+    });
+});
