18f7473c392d9226b6a1bd1ee78b1dbd20368aa4
Pontus Persson <pontus.alexander@gmail.com>
2016-09-20 20:32:04 +0200

    Add Session class, sessions map and react to create-session message.

--- server/main.js
+++ server/main.js
@@ -2,11 +2,28 @@ const WebSocketServer = require('ws').Server;
 
 const server = new WebSocketServer({port: 9000});
 
+const sessions = new Map;
+
+class Session
+{
+    constructor(id)
+    {
+        this.id = id;
+    }
+}
+
 server.on('connection', conn => {
     console.log('Connection established');
 
     conn.on('message', msg => {
         console.log('Message received', msg);
+
+        if (msg === 'create-session') {
+            const session = new Session('foobar');
+            sessions.set(session.id, session);
+        }
+
+        console.log(sessions);
     });
 
     conn.on('close', () => {
