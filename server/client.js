493c9e499433d405df4ecfee1cd09b2abd5604b3
Pontus Persson <pontus.alexander@gmail.com>
2016-09-20 21:12:09 +0200

    Add send method to Client.

--- server/client.js
+++ server/client.js
@@ -5,6 +5,12 @@ class Client
         this.conn = conn;
         this.session = null;
     }
+
+    send(msg)
+    {
+        console.log(`Sending message ${msg}`);
+        this.conn.send(msg)
+    }
 }
 
 module.exports = Client;
