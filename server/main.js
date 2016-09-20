ec2e41d15ace62479f2ae542565a2bfdd9575909
Pontus Persson <pontus.alexander@gmail.com>
2016-09-20 20:48:36 +0200

    Keep track of clients and clean sessions.
    * Add join and leave methods to session.
    * Set up Client when connection established.
    * Join client with new session on creation.
    * Leave session on disconnect.
    * Clean up session if empty.

--- server/main.js
+++ server/main.js
@@ -9,6 +9,32 @@ class Session
     constructor(id)
     {
         this.id = id;
+        this.clients = new Set;
+    }
+    join(client)
+    {
+        if (client.session) {
+            throw new Error('Client already in session');
+        }
+        this.clients.add(client);
+        client.session = this;
+    }
+    leave(client)
+    {
+        if (client.session !== this) {
+            throw new Error('Client not in session');
+        }
+        this.clients.delete(client);
+        client.session = null;
+    }
+}
+
+class Client
+{
+    constructor(conn)
+    {
+        this.conn = conn;
+        this.session = null;
     }
 }
 
@@ -22,6 +48,7 @@ function createId(len = 6, chars = 'abcdefghjkmnopqrstvwxyz01234567890') {
 
 server.on('connection', conn => {
     console.log('Connection established');
+    const client = new Client(conn);
 
     conn.on('message', msg => {
         console.log('Message received', msg);
@@ -29,6 +56,7 @@ server.on('connection', conn => {
         if (msg === 'create-session') {
             const id = createId();
             const session = new Session(id);
+            session.join(client);
             sessions.set(session.id, session);
         }
 
@@ -37,5 +65,12 @@ server.on('connection', conn => {
 
     conn.on('close', () => {
         console.log('Connection closed');
+        const session = client.session;
+        if (session) {
+            session.leave(client);
+            if (session.clients.size === 0) {
+                sessions.delete(session.id);
+            }
+        }
     });
 });
